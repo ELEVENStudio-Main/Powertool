d1be955fef41f9014f34183579121122aca18b7b
lijunhao2023 <60656888+ELEVENStudio-Main@users.noreply.github.com>
2021-02-18 15:19:02 +0800

    Add files via upload

---
+++ Core Code.cpp
@@ -0,0 +1,4 @@
+	SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
+	(NULL,"open","C:\\Program Files (x86)\\Mythware\\极域电子教室软件 v4.0 2015 豪华版\\StudentMain.exe",NULL,NULL,SW_SHOW);
+	reg delete
+	SM_MINIMIZE
